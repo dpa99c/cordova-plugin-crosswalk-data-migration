14de45cea697d1b4699b2e865f61bc9dbb3d1069
Ing. Ariel Alberto Grassano <arielectron@gmail.com>
2022-03-22 23:39:30 -0300

    Add cold restart

--- src/Migration.java
+++ src/Migration.java
@@ -5,13 +5,19 @@ package cordova.plugins.crosswalk;
  */
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
 
+import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+
+import android.content.pm.PackageManager;
 
 import java.io.File;
 
@@ -52,11 +58,15 @@ public class Migration extends CordovaPlugin {
     private File XWalkRoot;
     private File webviewRoot;
 
+    protected Context applicationContext;
+
     public Migration() {}
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         Log.d(TAG, "initialize()");
+
+        applicationContext = this.cordova.getActivity().getApplicationContext();
 
         if(!hasRun){
             hasRun = true;
@@ -155,7 +165,7 @@ public class Migration extends CordovaPlugin {
 
     private void restartCordova(){
         Log.d(TAG, "restarting Cordova activity");
-        activity.recreate();
+        this.doColdRestart();
     }
 
 
@@ -201,4 +211,69 @@ public class Migration extends CordovaPlugin {
 
         fileOrDirectory.delete();
     }
+
+    /**
+     * Performs a full cold app restart - restarts application
+     * https://stackoverflow.com/a/22345538/777265
+     */
+    protected void doColdRestart() {
+        String baseError = "Unable to cold restart application: ";
+        try {
+            Log.d(TAG, "Cold restarting application");
+            Context c = applicationContext;
+            //check if the context is given
+            if (c != null) {
+                //fetch the packagemanager so we can get the default launch activity
+                // (you can replace this intent with any other activity if you want
+                PackageManager pm = c.getPackageManager();
+                //check if we got the PackageManager
+                if (pm != null) {
+                    //create the intent with the default start activity for your application
+                    Intent mStartActivity = pm.getLaunchIntentForPackage(
+                            c.getPackageName()
+                    );
+                    if (mStartActivity != null) {
+                        //mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        //create a pending intent so the application is restarted after System.exit(0) was called.
+                        // We use an AlarmManager to call this intent in 100ms
+                        int mPendingIntentId = 223344;
+                        PendingIntent mPendingIntent = PendingIntent
+                                .getActivity(c, mPendingIntentId, mStartActivity,
+                                        PendingIntent.FLAG_CANCEL_CURRENT);
+                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
+                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+                        Log.i(TAG, "Killing application for cold restart");
+                        //kill the application
+                        System.exit(0);
+                    } else {
+                        handleError(baseError+"StartActivity is null");
+                    }
+                } else {
+                    handleError(baseError+"PackageManager is null");
+                }
+            } else {
+                handleError(baseError+"Context is null");
+            }
+        } catch (Exception ex) {
+            handleError(baseError+ ex.getMessage());
+        }
+    }
+
+    /**
+     * Handles an error while executing a plugin API method  in the specified context.
+     * Calls the registered Javascript plugin error handler callback.
+     * @param errorMsg Error message to pass to the JS error handler
+     */
+    public void handleError(String errorMsg){
+        try {
+            logError(errorMsg);
+        } catch (Exception e) {
+            logError(e.toString());
+        }
+    }
+
+    public void logError(String msg){
+        Log.e(TAG, msg);
+    }
+
 }
